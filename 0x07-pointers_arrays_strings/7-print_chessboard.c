1bd923aeadaa30fc8713db5f10aee05759f031e5
me <me.email.com>
2023-03-07 00:48:50 +0200

    l

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: two dimension array to print
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+7-print_chessboard.c [unix] (01:59 01/01/1970)       0,0-1 All
+"7-print_chessboard.c" [New]
+
